53fce4b4b2009c3ff2f73cd6168025656529934b
Shin Sangwoo <bano112@naver.com>
2024-06-07 18:04:47 +0900

    create / delete test

--- life_coding/index.js
+++ life_coding/index.js
@@ -6,6 +6,38 @@ const client = supabase.createClient(supabaseUrl, supabaseKey);
 async function refreshHistory() {
   let { data: record, error } = await client.from("page").select("*");
   console.log("record", record);
+
+  let tag = "";
+  for (let i = 0; i < record.length; i++) {
+    tag += `<div class="box"><h2>${record[i].title}</h2> - ${record[i].body}</div>`;
+  }
+  document.querySelector("#history").innerHTML = tag;
 }
 
 refreshHistory();
+
+async function recordHandler() {
+  const { data, error } = await client.from("page").insert([
+    {
+      title: prompt("Enter the title"),
+      body: prompt("Enter the body"),
+    },
+  ]);
+  refreshHistory();
+}
+
+async function DeleteHandler() {
+  const { data, error } = await client
+    .from("page")
+    .delete()
+    .match({ title: prompt("Enter the title") });
+  refreshHistory();
+}
+
+document.querySelector("#create_btn").addEventListener("click", () => {
+  recordHandler();
+});
+
+document.querySelector("#delete_btn").addEventListener("click", () => {
+  DeleteHandler();
+});
